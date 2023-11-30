9794a73474654f52d42100221145726c91c0838a
Linaeus14 <titodqrmawan123@gmail.com>
2023-11-30 10:46:30 +0800

    fix: list tile widget structure

--- lib/ui/widget/widget.dart
+++ lib/ui/widget/widget.dart
@@ -6,5 +6,4 @@ import 'package:pa_mobile/service/service.dart';
 part './button_primary.dart';
 part './button_secondary.dart';
 part './search_field.dart';
-part 'list_tile.dart';
 part './plant_list.dart';
