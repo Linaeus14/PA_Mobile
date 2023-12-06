32149a6b199337d7169b6268c6bb4b7628f326bf
sbanz <sabanz0210@gmail.com>
2023-12-06 21:28:07 +0800

    feat : add detail_page

---
+++ lib/ui/page/detail_page.dart
@@ -0,0 +1,66 @@
+part of './page.dart';
+
+class DetailPage extends StatelessWidget {
+  DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme scheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Container(
+                  margin: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: scheme.primary,
+                  ),
+                ),
+                Expanded(
+                    child: Container(
+                  margin: const EdgeInsets.all(24),
+                  child: Text(
+                    'Plant Name',
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                ))
+              ]),
+              const SizedBox(height: 20),
+              Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage("assets/logo.png"),
+                  fit: BoxFit.cover,
+                )),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                constraints: BoxConstraints(
+                    minHeight: MediaQuery.of(context).size.height - 347),
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0)),
+                  color: scheme.primary,
+                ),
+                child: Text(
+                  "DESKRIPSI",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
