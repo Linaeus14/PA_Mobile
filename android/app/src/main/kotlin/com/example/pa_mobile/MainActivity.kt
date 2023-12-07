d4f8583a81710003738da9221c4dbc82235596ae
Linaeus14 <titodqrmawan123@gmail.com>
2023-12-07 16:52:27 +0800

    fix : kotlin mainactivity

--- android/app/src/main/kotlin/com/example/pa_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pa_mobile/MainActivity.kt
@@ -1,13 +1,12 @@
-package com.example.pa_mobile;
+package com.example.pa_mobile
 
-import android.content.Context;
-import androidx.multidex.MultiDex;
-import io.flutter.embedding.android.FlutterActivity;
+import android.content.Context
+import androidx.multidex.MultiDex
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity extends FlutterActivity {
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
+class MainActivity : FlutterActivity() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
